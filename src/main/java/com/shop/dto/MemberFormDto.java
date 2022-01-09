d26732f12721b1c15d6fac0f281d627b75cbe742
문영 <ansyoung126@gmail.com>
2022-01-09 19:24:54 +0900

    회원 가입 기능 구현

--- src/main/java/com/shop/dto/MemberFormDto.java
+++ src/main/java/com/shop/dto/MemberFormDto.java
@@ -2,15 +2,27 @@ package com.shop.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Getter @Setter
 public class MemberFormDto {
 
+    @NotBlank(message = "이름을 입력해주세요.")
     private String name;
 
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
 
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
     private String password;
 
+    @NotBlank(message = "주소를 입력해주세요")
     private String address;
+
+    //책에서는 name 빼고 나머지 어노테이션 NotEmpty로 해줬지만, 나머지도 빈 문자열 검사해줘야햘거 같아서 모두 NotBlank로 설정
 }
