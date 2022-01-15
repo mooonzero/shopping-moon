2f15dd6bcd0501219e4ac0c073256e4d8c5c4e4c
문영 <ansyoung126@gmail.com>
2022-01-15 18:07:09 +0900

    spring sequrity(권한 설정)

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,7 +35,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
                 .logoutSuccessUrl("/");
 
+        http.authorizeRequests()
+                .mvcMatchers("/","/members/**","/item/**","/images/**")
+                .permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        //http.authorizeRequests() : sequrity처리에 HttpServletRequest를 이용하겠다.
+        //permitAll() : 모든 사용자가 인증(로그인) 없이 해당 경로에 접근할 수 있도록 설정
+        //mvcMatchers("/admin/**").hasRole("ADMIN") : /admin으로 시작하는 경로는 해당 계정이 ADMIN Role일 경우에만 접근 가능하도록 설정
+        //anyRequest().authenticated() : 위에서 설정해준 경로를 제외한 나머지 경로들은 모드 인증을 요구하도록 설정
+
+        http.exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+        //인증되지 않은 사용자가 리소스에 접근하였을 때 수행되는 핸들러를 등록
     }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
 
@@ -52,5 +67,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
               .passwordEncoder(passwordEncoder());
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        //static 디렉토리의 하위 파일은 인증을 무시하도록 설정
+        web.ignoring().antMatchers("/css/**","/js/**","/img/**");
+    }
 
 }
--- src/main/java/com/shop/entity/Member.java
+++ src/main/java/com/shop/entity/Member.java
@@ -44,7 +44,7 @@ public class Member {
         String password = passwordEncoder.encode(memberFormDto.getPassword());
         // spring security 설정 class에 등록한 BCryptPasswordEncoder Bean을 파라미터로 넘겨서 비밀번호를 암호화 해줌.
         member.setPassword(password);
-        member.setRole(Role.USER);
+        member.setRole(Role.ADMIN);
         return member;
     }
 }
