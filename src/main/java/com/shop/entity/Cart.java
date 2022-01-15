9c9695a95301d154eeb6ad3e0c50eef786e500c6
문영 <ansyoung126@gmail.com>
2022-01-15 19:20:36 +0900

    entity mapping

---
+++ src/main/java/com/shop/entity/Cart.java
@@ -0,0 +1,26 @@
+package com.shop.entity;
+//장바구니 entity 설계
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart")
+@Getter @Setter
+@ToString
+public class Cart {
+
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    //@JoinColumn의 name을 명시하지 않으면 JPA가 알아서 ID를 찾아줌.
+}
