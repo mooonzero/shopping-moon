e6a6d4deff10cf7d74e21646892fccd6c55d2895
문영 <ansyoung126@gmail.com>
2022-01-25 23:34:19 +0900

    update ItemImg

--- src/main/java/com/shop/service/ItemImgService.java
+++ src/main/java/com/shop/service/ItemImgService.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -40,5 +42,29 @@ public class ItemImgService {
         // imgUrl : 업로드 결과 로컬에 저장된 상품 이미지 파일을 불러오는 경우
         itemImg.updateItemImg(oriImgName, imgName, imgUrl);
         itemImgRepository.save(itemImg);
+    }
+
+    public void updateItemImg(Long itemImgId, MultipartFile itemImgFile)throws Exception{
+
+        //상품 이미지를 수정한 겨웅 상품 이미지 업데이트
+        if(!itemImgFile.isEmpty()){
+            // 상품 이미지 아이디를 이용하여 기존에 저장했던 상품 이미지 엔티티를 조회
+            ItemImg savedItemImg = itemImgRepository.findById(itemImgId).orElseThrow(EntityNotFoundException::new);
+
+            //기존 이미지 파일 삭제
+            //기존에 등록된 상품 이미지 파일이 있을 경우 해당 파일 삭제
+            if(!StringUtils.isEmpty(savedItemImg.getImgName())){
+
+                fileService.deleteFile(itemImgLocation+"/"+savedItemImg.getImgName());
+            }
+
+            String oriImgName = itemImgFile.getOriginalFilename();
+            //업데이트한 상품 이미지 파일 업로드
+            String imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+            String imgUrl = "/images/item/" + imgName;
+            //변경된 상품 이미지 정보를 세팅
+            //p.260 다시 이해하기
+            savedItemImg.updateItemImg(oriImgName, imgName, imgUrl);
+        }
     }
 }
